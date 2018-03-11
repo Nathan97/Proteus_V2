c56bf33b3097eaa16c6561a7e80904cb0ca80ffc
Nathan <hall.nathan97@gmail.com>
2018-03-11 17:37:38 +0000

    Imported sql.js lib

---
+++ Proteus_V2/Proteus_V2/js/sqljs/sql-requirejs.js
@@ -0,0 +1,15 @@
+﻿require.config({
+    baseUrl: '../../lib/sql.js/js/'
+});
+require(['sql'], function success(SQL) {
+    if (typeof SQL !== 'object') {
+        document.body.style.backgroundColor = 'red';
+        alert("Failed to require sql.js through AMD");
+    } else {
+        document.body.style.backgroundColor = 'green';
+        alert("sql.js successfully loaded with AMD");
+    }
+}, function error(err) {
+    console.log(err);
+    alert("Module load failed");
+});
